4cfd58a9093204f56776549dcfb21ef766600506
ChihoLi <124877629+ChihoLi@users.noreply.github.com>
2023-02-16 18:21:43 +0700

    Create program

--- Simple_paint_program.py
+++ Simple_paint_program.py
@@ -4,12 +4,36 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
 class Simple_paint_program(QWidget):
-    def __init__(self):
-        QWidget.__init__(self, None)
-        self.setWindowTitle("Simple paint program")
-    
-    def paintEvent(self, e):
-        p = QPainter()
+   def __init__(self):
+        super().__init__()
+
+        self.label = QtWidgets.QLabel()
+        canvas = QtGui.QPixmap(400, 300)
+        canvas.fill(Qt.GlobalColor.white)
+        self.label.setPixmap(canvas)
+        self.setCentralWidget(self.label)
+
+        self.last_x, self.last_y = None, None
+
+    def mouseMoveEvent(self, e):
+        if self.last_x is None: # First event.
+            self.last_x = e.position().x()
+            self.last_y = e.position().y()
+            return # Ignore the first time.
+
+        canvas = self.label.pixmap()
+        painter = QtGui.QPainter(canvas)
+        painter.drawLine(self.last_x, self.last_y, e.position().x(), e.position().y())
+        painter.end()
+        self.label.setPixmap(canvas)
+
+        # Update the origin for next time.
+        self.last_x = e.position().x()
+        self.last_y = e.position().y()
+
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
 
 def main():
     app = QApplication(sys.argv)
