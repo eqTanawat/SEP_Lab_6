6e0900f6739390feb613ef4e1943ed7ae754ee81
mid-per <90703846+mid-per@users.noreply.github.com>
2023-02-16 17:09:09 +0700

    Update Simple_drawing_window.py

--- Simple_drawing_window.py
+++ Simple_drawing_window.py
@@ -4,8 +4,8 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
 class Simple_drawing_window(QWidget):
-    def init(self):
-        QWidget.init(self,None)
+    def __init__(self):
+        QWidget.__init__(self,None)
         self.setWindowTitle("Simple GitHub Drawing")
         self.rabbit = QPixmap("images/rabbit.png")
 
